3179227c10a558936e4a4cc6912b315ac5def249
troymuma <tmuma@redhat.com>
2017-04-05 11:30:53 -0500

    Changes to make this pipeline work with the TKE jenkins

--- cicd/pipeline-basic/pipeline.groovy
+++ cicd/pipeline-basic/pipeline.groovy
@@ -9,7 +9,7 @@ String ocpApiServer = env.OCP_API_SERVER ? "${env.OCP_API_SERVER}" : "https://op
 
 node('master') {
 
-  env.NAMESPACE = readFile('/var/run/secrets/kubernetes.io/serviceaccount/namespace').trim()
+  env.NAMESPACE = "ticket-monster-dev"
   env.TOKEN = readFile('/var/run/secrets/kubernetes.io/serviceaccount/token').trim()
   env.OC_CMD = "oc --token=${env.TOKEN} --server=${ocpApiServer} --certificate-authority=/run/secrets/kubernetes.io/serviceaccount/ca.crt --namespace=${env.NAMESPACE}"
 
@@ -19,13 +19,13 @@ node('master') {
   env.STAGE2 = "${projectBase}-stage"
   env.STAGE3 = "${projectBase}-prod"
 
-  sh(returnStdout: true, script: "${env.OC_CMD} get is jenkins-slave-image-mgmt --template=\'{{ .status.dockerImageRepository }}\' -n openshift > /tmp/jenkins-slave-image-mgmt.out")
+  sh(returnStdout: true, script: "${env.OC_CMD} get is jenkins-slave-image-mgmt --template=\'{{ .status.dockerImageRepository }}\' -n jenkins-new > /tmp/jenkins-slave-image-mgmt.out")
   env.SKOPEO_SLAVE_IMAGE = readFile('/tmp/jenkins-slave-image-mgmt.out').trim()
   println "${env.SKOPEO_SLAVE_IMAGE}"
 
 }
 
-node('maven') {
+node('jdk8') {
 //  def artifactory = Artifactory.server(env.ARTIFACTORY_SERVER)
   // def artifactoryMaven = Artifactory.newMavenBuild()
   // def buildInfo = Artifactory.newBuildInfo()
@@ -48,16 +48,13 @@ node('maven') {
 
     sh """
        rm -rf oc-build && mkdir -p oc-build/deployments
-
        for t in \$(echo "jar;war;ear" | tr ";" "\\n"); do
          cp -rfv ./target/*.\$t oc-build/deployments/ 2> /dev/null || echo "No \$t files"
        done
-
        for i in oc-build/deployments/*.war; do
           mv -v oc-build/deployments/\$(basename \$i) oc-build/deployments/ROOT.war
           break
        done
-
        ${env.OC_CMD} start-build ${env.APP_NAME} --from-dir=oc-build --wait=true --follow=true || exit 1
     """
   }
@@ -100,31 +97,21 @@ podTemplate(label: 'promotion-slave', cloud: 'openshift', containers: [
   containerTemplate(name: 'jenkins-slave-image-mgmt', image: "${env.SKOPEO_SLAVE_IMAGE}", ttyEnabled: true, command: 'cat'),
   containerTemplate(name: 'jnlp', image: 'jenkinsci/jnlp-slave:2.62-alpine', args: '${computer.jnlpmac} ${computer.name}')
 ]) {
-
   node('promotion-slave') {
-
     stage("Promote To ${env.STAGE3}") {
-
       container('jenkins-slave-image-mgmt') {
         sh """
-
         set +x
         imageRegistry=\$(${env.OC_CMD} get is ${env.APP_NAME} --template='{{ .status.dockerImageRepository }}' -n ${env.STAGE2} | cut -d/ -f1)
-
         strippedNamespace=\$(echo ${env.NAMESPACE} | cut -d/ -f1)
-
         echo "Promoting \${imageRegistry}/${env.STAGE2}/${env.APP_NAME} -> \${imageRegistry}/${env.STAGE3}/${env.APP_NAME}"
         skopeo --tls-verify=false copy --remove-signatures --src-creds openshift:${env.TOKEN} --dest-creds openshift:${env.TOKEN} docker://\${imageRegistry}/${env.STAGE2}/${env.APP_NAME} docker://\${imageRegistry}/${env.STAGE3}/${env.APP_NAME}
         """
       }
     }
-
     stage("Verify Deployment to ${env.STAGE3}") {
-
       openshiftVerifyDeployment(deploymentConfig: "${env.APP_NAME}", namespace: "${STAGE3}", verifyReplicaCount: true)
-
     }
-
   }
 }
 */
